38ef34779e429970e47b14a75bd87a109bd9e48d
Alex Lewis <alexvlewis@gmail.com>
2022-11-16 01:37:06 -0800

    UI cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -39,14 +39,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`pub: ${state.pub} <br> priv: ${state.priv}`}
-        </a>
+        <p>{`pub: ${state.pub}`}</p>
+        <p>{`priv: ${state.priv}`}</p>
       </header>
     </div>
   );
