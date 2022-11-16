019feb9373713921cd1d9115dd1eef679bbeed9b
Alex Lewis <alexvlewis@gmail.com>
2022-11-16 02:34:01 -0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,26 @@ function App() {
 
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "libdogecoin-web.js";
-    script.id = "libdogecoin";
+    script.src = "dogecoin-js.js";
+    script.id = "dogecoin-js";
     document.body.appendChild(script);
     script.onload = async () => {
-      const libdogecoin: any = await window.loadWASM();
-      libdogecoin._dogecoin_ecc_start();
-
-      const privatePtr = libdogecoin.allocate(
-        libdogecoin.intArrayFromString(""),
-        libdogecoin.ALLOC_NORMAL
-      );
-      const publicPtr = libdogecoin.allocate(
-        libdogecoin.intArrayFromString(""),
-        libdogecoin.ALLOC_NORMAL
-      );
-
-      libdogecoin._generatePrivPubKeypair(privatePtr, publicPtr, false);
-
-      const privKey = `${libdogecoin.UTF8ToString(privatePtr)}`;
-      const pubKey = `${libdogecoin.UTF8ToString(publicPtr)}`;
-
-      libdogecoin._dogecoin_ecc_stop();
-
-      setState({ pub: pubKey, priv: privKey });
+      console.log("script loaded");
+      // const libdogecoin: any = await window.loadWASM();
+      // libdogecoin._dogecoin_ecc_start();
+      // const privatePtr = libdogecoin.allocate(
+      //   libdogecoin.intArrayFromString(""),
+      //   libdogecoin.ALLOC_NORMAL
+      // );
+      // const publicPtr = libdogecoin.allocate(
+      //   libdogecoin.intArrayFromString(""),
+      //   libdogecoin.ALLOC_NORMAL
+      // );
+      // libdogecoin._generatePrivPubKeypair(privatePtr, publicPtr, false);
+      // const privKey = `${libdogecoin.UTF8ToString(privatePtr)}`;
+      // const pubKey = `${libdogecoin.UTF8ToString(publicPtr)}`;
+      // libdogecoin._dogecoin_ecc_stop();
+      // setState({ pub: pubKey, priv: privKey });
     };
   }, [setState]);
 
